4e087d054ec3d3aab024366320bef4842afef0f8
Gârcineanu Elena-Laura <57571504+ElenaLaura366@users.noreply.github.com>
2025-04-02 18:39:06 +0300

    Made the UI for student page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,15 @@ import { LoginComponent } from './login/login.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { StudentProfileComponent } from './student-profile/student-profile.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, 
   { path: 'home', component: HomePageComponent, pathMatch: 'full' }, 
   { path: 'login', component: LoginComponent },
   { path: 'catalogue', component: CatalogueComponent },
-  { path: 'reset-password', component: ResetPasswordComponent }
-
+  { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'student-profile', component: StudentProfileComponent },
 ];
 
 export const RoutingModule = RouterModule.forRoot(appRoutes);
